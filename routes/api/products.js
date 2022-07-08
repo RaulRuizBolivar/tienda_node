d042ae86163c1c6c98d8c511ae9fd9432b01be7e
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-07-08 20:03:09 +0200

    Ya no soy gilipollas prometiendo

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,32 +1,38 @@
-const express = require("express")
+const express = require( "express" )
 const router = express.Router()
 
-const Product = require("../../models/product.model")
-router.get("/", (req, res) => {
+const Product = require( "../../models/product.model" )
+
+
+router.get( "/", ( req, res ) => {
   Product.find()
-    .then(products => res.json(products))
-    .catch(err => res.json({ error: err.message }))
-})
+    .then( products => res.json( products ) )
+    .catch( err => res.json( { error: err.message } ) )
+} )
 
-router.get("/:price", async (req, res) => {
+router.get( "/:price", ( req, res ) => {
   const { price } = req.params
-  await Product.find({
+  Product.find( {
     price: { $gt: price },
-  })
-    .then(products => res.json(products))
-    .catch(err => res.json({ error: err.message }))
-})
+  } )
+    .then( products => res.json( products ) )
+    .catch( err => res.json( { error: err.message } ) )
+} )
 
-router.post("/", async (req, res) => {
+router.post( "/", ( req, res ) => {
   const { body } = req
-  await Product.create(body)
-    .then(newProduct => res.status(201).json(newProduct))
-    .catch(err => res.status(500).json({ error: err.message }))
-})
+  Product.create( body )
+    .then( newProduct => res.status( 201 ).json( newProduct ) )
+    .catch( err => res.status( 500 ).json( { error: err.message } ) )
+} )
 
-router.put("/:id", async (req, res) => {
-  const { id } = req.params
-  const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
-  res.json(product)
-})
+router.put( "/:id", async ( req, res ) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate( id, req.body, { new: true } )
+    res.json( product )
+  } catch ( err ) {
+    res.json( { error: err.message } )
+  }
+} )
 module.exports = router
--- tests/products.spec.js
+++ tests/products.spec.js
@@ -1,35 +1,34 @@
-const request = require("supertest")
-const app = require("../app")
-const mongoose = require("mongoose")
-const { response } = require("express")
-const Producto = require("../models/product.model")
+const request = require( "supertest" )
+const app = require( "../app" )
+const mongoose = require( "mongoose" )
+const Producto = require( "../models/product.model" )
 
-describe("Products Tests", () => {
+describe( "Products Tests", () => {
   // Antes de lanzar todas las pruebas, debo conectarme a la base de datos
-  beforeAll(async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/tienda_online")
-  })
+  beforeAll( async () => {
+    await mongoose.connect( "mongodb://127.0.0.1:27017/tienda_online" )
+  } )
   // Despues de lanzar todas las pruebas, debo desconectarme de la base de
   // datos
-  afterAll(async () => {
+  afterAll( async () => {
     await mongoose.disconnect()
-  })
-  describe("GET /api/products", () => {
+  } )
+  describe( "GET /api/products", () => {
     let response
-    beforeAll(async () => {
-      response = await request(app).get("/api/products").send()
-    })
-    it("deberia devolver un status 200", () => {
-      expect(response.statusCode).toBe(200)
-    })
-    it("deberia devolver un objeto en formato JSON", () => {
-      expect(response.headers["content-type"]).toContain("json")
-    })
-    it("deberia devolver un array", () => {
-      expect(response.body).toBeInstanceOf(Array)
-    })
-  })
-  describe("POST /api/products", () => {
+    beforeAll( async () => {
+      response = await request( app ).get( "/api/products" ).send()
+    } )
+    it( "deberia devolver un status 200", () => {
+      expect( response.statusCode ).toBe( 200 )
+    } )
+    it( "deberia devolver un objeto en formato JSON", () => {
+      expect( response.headers[ "content-type" ] ).toContain( "json" )
+    } )
+    it( "deberia devolver un array", () => {
+      expect( response.body ).toBeInstanceOf( Array )
+    } )
+  } )
+  describe( "POST /api/products", () => {
     /**
      * Antes de cada prueba, realizar la peticion de tipo POST sobre /api/products
      * Probamos si la respuesta devuelve status 201
@@ -45,24 +44,24 @@ describe("Products Tests", () => {
       price: 20,
     }
     let response
-    beforeAll(async () => {
-      response = await request(app).post("/api/products").send(newProduct)
-    })
-    afterAll(async () => {
-      await Producto.deleteMany({
+    beforeAll( async () => {
+      response = await request( app ).post( "/api/products" ).send( newProduct )
+    } )
+    afterAll( async () => {
+      await Producto.deleteMany( {
         department: "test",
-      })
-    })
-    it("deberia devolver un status 201", () => {
-      expect(response.statusCode).toBe(201)
-    })
-    it("deberia responderme con un JSON", () => {
-      expect(response.headers["content-type"]).toContain("json")
-    })
-    it("deberia devolverme el atributo _id en la respuesta", () => {
-      expect(response.body._id).toBeDefined()
-    })
-  })
+      } )
+    } )
+    it( "deberia devolver un status 201", () => {
+      expect( response.statusCode ).toBe( 201 )
+    } )
+    it( "deberia responderme con un JSON", () => {
+      expect( response.headers[ "content-type" ] ).toContain( "json" )
+    } )
+    it( "deberia devolverme el atributo _id en la respuesta", () => {
+      expect( response.body._id ).toBeDefined()
+    } )
+  } )
   /**
    *
    * Pruebas
@@ -78,7 +77,7 @@ describe("Products Tests", () => {
    *
    * */
 
-  describe("PUT /api/products/:id", () => {
+  describe( "PUT /api/products/:id", () => {
     let newProduct = {
       name: "teclado",
       description: "Para programar las cosicas",
@@ -92,26 +91,28 @@ describe("Products Tests", () => {
       stock: 200,
     }
     let response
-    let id
-    beforeEach(async () => {
-      const productToEdit = await Producto.create(newProduct)
-      id = productToEdit._id
-      response = await request(app)
-        .put("api/products/" + id)
-        .send(edit)
-    })
-    afterEach(async () => {
-      await Producto.findByIdAndDelete(id)
-    })
-    it("deberia devolver un status 200", () => {
-      expect(response.statusCode).toBe(200)
-    })
-    it("deberia devolver un json", () => {
-      expect(response.headers["content-type"]).toContain("json")
-    })
-    it("El objeto devuelto debe contener los atributos editados", () => {
-      expect(response.body.price).toBe(45)
-      expect(response.body.stock).toBe(200)
-    })
-  })
-})
+    let productToEdit
+    beforeEach( async () => {
+      productToEdit = await Producto.create( newProduct )
+      response = await request( app )
+        .put( "/api/products/" + productToEdit._id )
+        .send( edit )
+    } )
+    afterEach( async () => {
+      await Producto.findByIdAndDelete( productToEdit._id )
+    } )
+    it( "deberia devolver un status 200", () => {
+      console.log( response )
+      expect( response.statusCode ).toBe( 200 )
+    } )
+    it( "deberia devolver un json", () => {
+      expect( response.headers[ "content-type" ] ).toContain( "json" )
+    } )
+    it( "El objeto devuelto debe contener los atributos editados", () => {
+      expect( response.body.price ).toBe( 45 )
+      expect( response.body.stock ).toBe( 200 )
+    } )
+  } )
+
+
+} )
